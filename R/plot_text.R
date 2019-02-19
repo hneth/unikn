9ce08b6fc222851056ac1bb906f9ea82dd95298b
hneth <hansneth@gmail.com>
2019-02-19 21:02:22 +0100

    finish box_text and add simpler highlight function

--- R/plot_text.R
+++ R/plot_text.R
@@ -5,7 +5,7 @@
 ## Plot text with formatting elements (highlighting or underlining).
 
 
-## box_text: Add text with a colored background box to a plot ------
+## box_text: Add text with a colored background box to a plot: ------
 
 ## Adapted from Ian Kopacka's solution to a question at: 
 ## https://stackoverflow.com/questions/45366243/text-labels-with-background-colour-in-r
@@ -28,36 +28,42 @@
 #' @param lbls A character vector specifying the text labels 
 #' to be written.
 #' 
-#' @param col_lbl The color(s) of the text label(s).
+#' @param col_lbl The color(s) of the text label(s). 
+#' Default: \code{col_lbl = "black"}. 
 #' 
-#' @param col_bg The color(s) to fill or shade the rectangle(s) with. 
-#' The default \code{col_bg = NA} implies no fill 
-#' (i.e., draws transparent rectangles).
+#' @param col_bg The color(s) to highlight or fill the rectangle(s) with. 
+#' Default: \code{col_bg = NA} (transparent rectangles).
 #' 
 #' @param col_bg_brd The color(s) for rectangle border(s). 
-#' The default \code{NA} omits borders.
+#' Default: \code{col_bg_brd = NA} (no borders).
 #' 
-#' @param adj One or two adjustment values in \code{[0, 1]} which specify 
-#' the x (and optionally y) adjustment of the labels.
+#' @param adj Adjustment of label position, 
+#' 1 or 2 adjustment values in \code{[0, 1]} which specify 
+#' the x (and optionally y) adjustment of the labels. 
+#' Default: \code{adj = NULL}. 
 #' 
-#' @param pos A position specifier for the text. If specified this overrides
+#' @param pos A position specifier for the text. If specified, this overrides
 #' any \code{adj} value given. Values of 1, 2, 3 and 4, respectively indicate
 #' positions below, to the left of, above and to the right of the specified
-#' coordinates.
+#' coordinates. 
+#' Default: \code{pos = 4} (right). 
 #' 
 #' @param offset When \code{pos} is specified, this value sets the offset of
-#' the label from the specified coordinate in fractions of a character width.
+#' the label from the specified coordinate in fractions of a character width. 
+#' Default: \code{offset = NULL}. 
 #' 
 #' @param padding A factor used for the padding of the box around
-#' the text. Padding is specified in fractions of a character width. If a
-#' vector of length 2 is specified then 2 different factors are used for the
-#' padding in x- and y-direction.
+#' the text (in fractions of a character width). 
+#' If a vector of length 2 is specified then 2 different factors 
+#' are used for padding in the x- and y-direction, respectively. 
+#' Default: \code{padding = c(.20, .80)}. 
 #' 
-#' @param cex A numeric character expansion factor, which is  
-#' multiplied by \code{par("cex")} to yield the final character size.
+#' @param cex A numeric character expansion factor, 
+#' multiplied by \code{par("cex")} to yield the character size. 
+#' Default: \code{cex = 2}. 
 #' 
 #' @param font The font to be used. 
-#' Default: \code{font = 2} (bold).
+#' Default: \code{font = 2} (i.e., bold).
 #' 
 #' @return Returns the coordinates of the background rectangle(s). 
 #' If multiple labels are placed in a vector then the coordinates are returned
@@ -67,28 +73,41 @@
 #' @author Adapted from code by Ian Kopacka.
 #' 
 #' @examples
-#' ## Create some noisy background:
-#' plot(x = runif(500), y = runif(500), type = "p", pch = 16, cex = 3, col = grey(.33, .10))
+#' ## Example 1: Simple highlights
+#' plot(x = 0, y = 0, type = "n", xlim = c(0, 1), ylim = c(0, 1))
+#' box_text(x = 0, y = .9, lbls = "Something to notice", col_bg = unlist(seeblau))
+#' box_text(x = 0, y = .7, lbls = "Highlighting text is really simple", cex = 1.5, col_bg = "gold")
+#' box_text(x = 0, y = .6, lbls = "and strikingly effective", cex = 1.5, col_bg = pal_seeblau[[1]])
 #' 
-#' ## Adjust cex, font and adj:
-#' box_text(x = .01, y = .95, lbls = "What a messy plot",
-#'          col_bg = "skyblue1", adj = c(0, 0), padding = c(.25, .75), cex = 1.8)
+#' ## Example 2: Messy plot
+#' n <- 20
+#' set.seed(1)
+#' plot(x = runif(n), y = runif(n), type = "p", pch = 16, cex = 20, col = grey(0, .20), axes = F, xlab = "", ylab = "")
 #' 
-#' ## Vector of 2 lbls, using argument 'pos' to position right of coordinates:
-#' box_text(x = c(.15, .60), y = c(.10, .50),
-#'          lbls = c("Some highlighted text here.", "Something else here."),
-#'          col_bg = c("skyblue2", "gold"), pos = 4, padding = c(.25, 1))
-#'    
+#' # Adjust cex, font and adj:
+#' box_text(x = .05, y = .90, lbls = "What a messy plot",
+#'          col_bg = seeblau[[1]], adj = c(0, 0), padding = c(.25, .85), cex = 2)
+#' 
+#' # Vector of 2 lbls, using argument 'pos' to position right of coordinates:
+#' box_text(x = c(.30, .60), y = c(.20, .40),
+#'          lbls = c("Note something here", "Some highlighting here"),
+#'          col_bg = c(pal_seeblau[[2]], "gold"), pos = 4, padding = c(.25, .85), cex = 1.2)
+#' 
+#' @family text functions
+#' 
+#' @seealso
+#' \code{\link{highlight}} (reduced version) to mark text with a colored box  
+#'                 
 #' @import graphics 
 #'                          
-#' @export 
+#'                          
 
 ## - Definition: ---- 
 
-box_text <- function(x, y, lbls = NA, # coordinates and labels of text element(s) 
-                     col_lbl = NULL, col_bg = NA, col_bg_brd = NA,  # colors
-                     adj = NULL, pos = NULL, offset = .5, padding = c(.5, .5),  # positions 
-                     cex = 1, font = 2  # text size and font
+box_text <- function(x, y, lbls = NA,               # coordinates and labels of text element(s) 
+                     col_lbl = "black", col_bg = NA, col_bg_brd = NA,         # colors
+                     adj = NULL, pos = 4, offset = 0, padding = c(.20, .80),  # positions 
+                     cex = 2, font = 2                                        # text size and font
 ){
   
   ## Coordinates: ----
@@ -182,19 +201,134 @@ box_text <- function(x, y, lbls = NA, # coordinates and labels of text element(s
 } # box_text end.
 
 ### Check: 
-# ## Create some noisy background:
-# plot(x = runif(500), y = runif(500), type = "p", pch = 16, cex = 3, col = grey(.33, .10))
+
+# ## Example 1: Simple highlights 
+# plot(x = 0, y = 0, type = "n", xlim = c(0, 1), ylim = c(0, 1))
+# box_text(x = 0, y = .9, lbls = "Something to notice", col_bg = unlist(seeblau))
+# box_text(x = 0, y = .7, lbls = "Highlighting text is really simple", cex = 1.5, col_bg = "gold")
+# box_text(x = 0, y = .6, lbls = "and strikingly effective", cex = 1.5, col_bg = pal_seeblau[[1]])
 # 
-# ## Adjust cex, font and adj:
-# box_text(x = .01, y = .95, lbls = "What a messy plot",
-#           col_bg = "skyblue1", adj = c(0, 0), padding = c(.25, .75), cex = 1.8)
+# ## Example 2: Messy plot 
+# n <- 20
+# set.seed(1)
+# plot(x = runif(n), y = runif(n), type = "p", pch = 16, cex = 20, col = grey(0, .20), axes = F, xlab = "", ylab = "")
 # 
-# ## Vector of 2 lbls, using argument 'pos' to position right of coordinates:
-# box_text(x = c(.15, .60), y = c(.10, .50),
-#          lbls = c("Some highlighted text here.", "Something else here."),
-#          col_bg = c("skyblue2", "gold"), pos = 4, padding = c(.25, 1))
+# # Adjust cex, font and adj:
+# box_text(x = .05, y = .90, lbls = "What a messy plot",
+#          col_bg = seeblau[[1]], adj = c(0, 0), padding = c(.25, .85), cex = 2)
+# 
+# # Vector of 2 lbls, using argument 'pos' to position right of coordinates:
+# box_text(x = c(.30, .60), y = c(.20, .40),
+#          lbls = c("Note something here", "Some highlighting here"),
+#          col_bg = c(pal_seeblau[[2]], "gold"), pos = 4, padding = c(.25, .85), cex = 1.2)
 
 
+## Reduced version (with fewer arguments):
+
+## highlight: Place and highlight text on plot: ------ 
+
+## - Documentation: ---- 
+
+#' \code{highlight} places 1 or more text strings (of a character vector \code{lbls}) 
+#' onto a plot and places a colored box behind
+#' each label to highlight it (i.e., make it stand out from the background).
+#' 
+#' \code{highlight} uses the base graphics system \code{graphics::}. 
+#' 
+#' @param x A numeric vector of x-coordinates at which the 
+#' text labels in \code{lbls} should be written. 
+#' If the length of \code{x} and \code{y} differs, the shorter one is recycled.
+#' 
+#' @param y A numeric vector of y-coordinates at which the 
+#' text labels in \code{lbls} should be written.
+#' 
+#' @param lbls A character vector specifying the text labels 
+#' to be written.
+#' 
+#' @param col_lbl The color(s) of the text label(s). 
+#' Default: \code{col_lbl = "black"}. 
+#' 
+#' @param col_bg The color(s) to highlight or fill the rectangle(s) with. 
+#' Default: \code{col_bg = NA} (transparent rectangles).
+#' 
+#' @param cex A numeric character expansion factor, 
+#' multiplied by \code{par("cex")} to yield the character size. 
+#' Default: \code{cex = 2}. 
+#' 
+#' @param font The font to be used. 
+#' Default: \code{font = 2} (i.e., bold).
+#' 
+#' 
+#' @examples
+#' ## Example 1: Simple highlights
+#' plot(x = 0, y = 0, type = "n", xlim = c(0, 1), ylim = c(0, 1), xlab = "", ylab = "")
+#' 
+#' highlight(x = 0, y = .9, lbls = "Something to notice")
+#' highlight(x = 0, y = c(.7, .6), 
+#'           lbls = c("Highlighting text is simple", "but strikingly effective"), 
+#'           cex = 1.5, col_bg = c(pal_seeblau[[2]], pal_seeblau[[1]]))
+#' highlight(x = .5, y = c(.4, .3), lbls = c("It is also flexible", "but should be handled with care"), cex = 1.2, 
+#'           col_lbl = c("white", "black"), col_bg = c(pal_seeblau[[5]], "gold"))
+#' 
+#' ## Example 2: Messy plot
+#' n <- 20
+#' set.seed(1)
+#' plot(x = runif(n), y = runif(n), type = "p", pch = 16, cex = 20, col = grey(0, .20), axes = F, xlab = "", ylab = "")
+#' 
+#' # Only 1 label:
+#' highlight(x = .05, y = .90, lbls = "What a messy plot")
+#' 
+#' # 2 labels at once:
+#' highlight(x = c(.30, .60), y = c(.20, .40),
+#'           lbls = c("Note something here", "Some highlighting here"),
+#'           col_bg = c(pal_seeblau[[2]], "gold"), cex = 1.2)
+#'           
+#' @family text functions
+#' 
+#' @seealso
+#' \code{\link{plot_box}} to plot text in a colored box 
+#'      
+#' @import graphics 
+#'                          
+#' @export 
+
+## - Definition: ---- 
+
+highlight <- function(x, y, lbls = NA,                             # coordinates and labels of text element(s) 
+                      col_lbl = "black", col_bg = unlist(seeblau), # color(s)
+                      cex = 2, font = 2                            # text size and font
+){
+  
+  # Pass on (to richer box_text function):
+  box_text(x = x, y = y, lbls = lbls, col_lbl = col_lbl, col_bg = col_bg, cex = cex, font = font)
+  
+}
+
+
+# ## Check:
+#
+# ## Example 1: Simple highlights
+# plot(x = 0, y = 0, type = "n", xlim = c(0, 1), ylim = c(0, 1), xlab = "", ylab = "")
+# 
+# highlight(x = 0, y = .9, lbls = "Something to notice")
+# highlight(x = 0, y = c(.7, .6), 
+#           lbls = c("Highlighting text is simple", "but strikingly effective"), 
+#           cex = 1.5, col_bg = c(pal_seeblau[[2]], pal_seeblau[[1]]))
+# highlight(x = .5, y = c(.4, .3), lbls = c("It is also flexible", "but should be handled with care"), cex = 1.2, 
+#           col_lbl = c("white", "black"), col_bg = c(pal_seeblau[[5]], "gold"))
+# 
+# ## Example 2: Messy plot
+# n <- 20
+# set.seed(1)
+# plot(x = runif(n), y = runif(n), type = "p", pch = 16, cex = 20, col = grey(0, .20), axes = F, xlab = "", ylab = "")
+# 
+# # Only 1 label:
+# highlight(x = .05, y = .90, lbls = "What a messy plot")
+# 
+# # 2 labels at once:
+# highlight(x = c(.30, .60), y = c(.20, .40),
+#           lbls = c("Note something here", "Some highlighting here"),
+#           col_bg = c(pal_seeblau[[2]], "gold"), cex = 1.2)
 
 
 ## ToDo: ------
