f3a97fe5dd2095e599a3d570dda3487bd0613484
hneth <hansneth@gmail.com>
2019-02-22 20:09:33 +0100

    add plot_mar (to test margin resizing)

--- R/plot_util.R
+++ R/plot_util.R
@@ -5,6 +5,49 @@
 ## Utility functions for plotting 
 ## (for internal use, not exported): 
 
+## plot_mar: Set plotting margins: ------ 
+
+plot_mar <- function(mar_all = 0,
+                     oma_all = 0){
+  
+  # Record graphical parameters (par):
+  opar <- par(no.readonly = TRUE)  # all par settings that can be changed.
+  on.exit(par(opar)) # restore original settings
+  
+  ## Plotting area: ----- 
+  
+  ## Margins (in lines): 
+  # mar_all <- 0  # all inner
+  # oma_all <- 0  # all outer
+  
+  par(mar = c(0, 0, 0, 0) + mar_all)      # margins; default: par("mar") = 5.1 4.1 4.1 2.1.
+  par(oma = c(0, 0, 0, 0) + oma_all)  # outer margins; default: par("oma") = 0 0 0 0.
+  
+  ## Plot:
+  plot(0, 0, type = "p")
+  grid()
+  points(x = 0, y = 0, pch = 3, cex = 2, col = "forestgreen")
+  text(x = 0, y = 0, labels = "Origin", cex = 1, font = 1, pos = 4, col = "firebrick")
+  
+  # Return:
+  invisible()
+  
+}
+
+## Check:
+# par("mar")  # => # 5.1 4.1 4.1 2.1 (default)
+# par("oma")  # => # 0   0   0   0   (default)
+
+## Outside of function:
+# plot(0, 0, type = "p")
+# grid()
+# points(x = 0, y = 0, pch = 3, cex = 2, col = "forestgreen")
+# text(x = 0, y = 0, labels = "Origin", cex = 1, font = 1, pos = 4, col = "firebrick")
+
+## Inside of function:
+# plot_mar()
+
+
 ## plot_grid: Helper function to plot a grid of points (to position objects): ------ 
 
 plot_grid <- function(x_min = 0, x_max = 1, y_min = 0, y_max = 1){
