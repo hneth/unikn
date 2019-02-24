7cca383b202985aee6038e0fe7ed03e95446550f
hneth <hansneth@gmail.com>
2019-02-24 14:34:03 +0100

    add file color_def.R and increment version

---
+++ R/color_def.R
@@ -0,0 +1,17 @@
+## color_def.R | unikn
+## hn |  uni.kn  |  2019 02 24
+## ---------------------------
+
+## Define colors and color palettes. 
+
+# Cleaner version of color_definitions.R, with fewer components and exports:
+# (1) official color palettes: NOT exported, but accessed via "seecol" user-interface function. 
+# (2) official preferred colors: Capitalized and exported (for convenient access).  
+# (+) Additional color palettes (unikn_plus/_pref/_light/_dark/_pair): Move to a separate/later file. 
+
+## ToDo: ------
+
+# - Export all preferred color values (as HEX character value) with Capital letter names!
+# - Do NOT export the other palettes and values, but use "seecol" function as user-interface. 
+
+## eof. ----------
