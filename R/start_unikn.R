cc4dbe27773014f465c704ff8336e9e3cf2d4ac3
hneth <hansneth@gmail.com>
2019-05-21 13:12:16 +0200

    add start_unikn.R

---
+++ R/start_unikn.R
@@ -0,0 +1,65 @@
+## start_unikn.R | unikn
+## spds | uni.kn |  2019 05 20
+## ---------------------------
+
+## Open package guide: -----------------------
+
+#' Opens the unikn package guides
+#'
+#' @importFrom utils vignette
+#' @importFrom utils browseVignettes
+#'
+#' @export
+
+unikn.guide <- function() {
+
+  # utils::vignette(topic = "User Guide", package = "unikn")
+  utils::browseVignettes(package = "unikn")
+
+}
+
+## Initialize package: ------
+
+.onAttach <- function(libname, pkgname) {
+
+  ## Welcome message: ------
+
+  pkg_version <- utils::packageVersion("unikn", lib.loc = NULL)
+  # welcome_message <- paste0("Welcome to unikn (v", pkg_version, ")!")
+  welcome_message <- paste0("Welcome to unikn!")
+
+  packageStartupMessage(welcome_message)
+
+  ## User guidance: ------
+
+  ## Roll unikn dice: ------
+  dice <- sample(1:3, 1)
+
+  if (dice == -77) {
+    pkg_version <- utils::packageVersion("unikn", lib.loc = NULL)
+    pkg_message <- paste0("Running unikn (v", pkg_version, ")...")
+
+    packageStartupMessage(" ")
+    packageStartupMessage(pkg_message)
+    packageStartupMessage(" ")
+  }
+
+  if (dice == -88) {
+    packageStartupMessage(" ")
+    packageStartupMessage("citation('unikn') provides citation info.")
+    packageStartupMessage(" ")
+  }
+
+  # all cases:
+  packageStartupMessage("unikn.guide() opens user guides.")
+
+}
+
+
+## Done: ------
+## - ...
+
+## ToDo: ------
+## - ...
+
+## eof. ----------
