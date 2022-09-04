ad6b2fcc982c939e283f68fcac3f9825dc74598b
Hansjörg Neth <hansneth@gmail.com>
2022-09-04 13:39:09 +0200

    add 2 startup hints

--- R/start_unikn.R
+++ R/start_unikn.R
@@ -37,30 +37,42 @@ unikn.guide <- function() {
   
   # 1. Probabilistic: ----
   
-  dice <- sample(1:8, 1)
-  cpen <- sample(c("in_bordeaux", "in_karpfenblau", "in_pinky"), size = 1)
-  
+  dice <- sample(1:10, 1)
+  pens <- c("in_bordeaux", "in_karpfenblau", "in_pinky")
+  pen1 <- sample(pens, size = 1)
+
   if (dice == 1){
     
-    packageStartupMessage(in_grau(sapply("seecol()", FUN = cpen), " shows colors or color palettes.", sep = ""))
+    packageStartupMessage(in_grau(sapply("seecol()", FUN = pen1), " shows colors or color palettes.", sep = ""))
     
   } else if (dice == 2){
     
-    packageStartupMessage(in_grau(sapply("usecol()", FUN = cpen), " allows changing and using colors.", sep = ""))
+    packageStartupMessage(in_grau(sapply("usecol()", FUN = pen1), " allows changing and using colors.", sep = ""))
     
   } else if (dice == 3) {
     
-    packageStartupMessage(in_grau(sapply("simcol()", FUN = cpen), " searches for similar colors.", sep = ""))
+    packageStartupMessage(in_grau(sapply("simcol()", FUN = pen1), " searches for similar colors.", sep = ""))
     
   } else if (dice == 4) {
     
-    packageStartupMessage(in_grau(sapply("grepal()", FUN = cpen), " searches for color names.", sep = ""))
+    packageStartupMessage(in_grau(sapply("grepal()", FUN = pen1), " searches for color names.", sep = ""))
     
   } else if (dice == 5) {
     
-    packageStartupMessage(in_grau(sapply("citation('unikn')", FUN = cpen), " provides citation information.", sep = ""))
+    packageStartupMessage(in_grau(sapply("shades_of()", FUN = pen1), " yields color gradients.", sep = ""))
     
-  } else if (dice == 6) {    
+  } else if (dice == 6) {
+    
+    packageStartupMessage(in_grau(sapply("citation('unikn')", FUN = pen1), " provides citation information.", sep = ""))
+    
+  } else if (dice == 7) {
+    
+    pen2 <- sample(pens[pens != pen1], size = 1)
+    
+    packageStartupMessage(in_grau(sapply("seecol()", FUN = pen1), " shows ", 
+                                  sapply("shades_of(5, 'grey0')", FUN = pen2), ".", sep = ""))
+    
+  } else if (dice == 8) {    
     
     packageStartupMessage(crayon::black(# "Colors:", 
                                         in_bordeaux("bordeaux"), 
@@ -73,15 +85,15 @@ unikn.guide <- function() {
                                         in_seegruen("seegruen"), 
                                         sep = " | "))  
     
-  } else if (dice == 7) {
+  } else if (dice == 9) {
     
   # Stroop task:
   packageStartupMessage(crayon::black("Try naming the colors of these words:"), sep = "")
   packageStartupMessage(in_pinky("black", in_karpfenblau("red"), in_bordeaux("green"), in_seegruen("blue"), in_peach("snow"), in_seeblau("yellow"), sep = "  "))
                         
-  } else { # 8: rest:
+  } else { # 10: rest:
     
-  packageStartupMessage(in_grau(sapply("unikn.guide()", FUN = cpen), " opens user guides.", sep = ""))
+  packageStartupMessage(in_grau(sapply("unikn.guide()", FUN = pen1), " opens user guides.", sep = ""))
     
   }
   
