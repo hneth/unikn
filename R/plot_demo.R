3814e1cca7c8445eedbeb05819a54c1d379fcb7b
Hansjörg Neth <hansneth@gmail.com>
2022-09-20 08:55:24 +0200

    add plot_polygon() as new demoplot() option

--- R/plot_demo.R
+++ R/plot_demo.R
@@ -1,5 +1,5 @@
 ## plot_demo.R | unikn
-## spds | uni.kn | 2022 09 19
+## spds | uni.kn | 2022 09 20
 ## ---------------------------
 
 ## Demo functions for color palettes.
@@ -22,7 +22,7 @@ set_seed <- function(seed){
 
 # (B) Individual functions: -----
 
-# plot_bar: Bar plot ---- 
+# plot_bar: A bar plot ---- 
 
 plot_bar <- function(pal, col_par = NULL, alpha = 1, 
                      n = 5,  # scaling: number of categories (for each color) 
@@ -163,7 +163,164 @@ plot_bar <- function(pal, col_par = NULL, alpha = 1,
 # plot_bar(pal_unikn_pref, n = 4, beside = F, horiz = T, as_prop = T, col_par = NA)
 
 
-# plot_table: Area/mosaic plot: ---- 
+
+# plot_polygon: A polygon/mountain range plot ---- 
+
+plot_polygon <- function(pal, col_par = NULL, alpha = 1, 
+                         n = 100,  # scaling: x-range
+                         # args with defaults:
+                         main = NULL,
+                         sub = NULL, 
+                         pal_name = NULL, 
+                         seed = NULL){
+  
+  # Prepare: ---- 
+  
+  # Parameters currently fixed:
+  axes <- FALSE
+  
+  # colors:
+  col_pal <- usecol(pal = pal, alpha = alpha)
+  n_col <- length(col_pal)
+  
+  if (n_col < 2){ # special case: 
+    col_pal <- c(col_pal, par("bg"))  # add bg color of device 
+    # col_pal <- c(par("col"), col_pal, par("bg"))  # add fg and bg color of device 
+    n_col   <- length(col_pal)
+  }
+  
+  # seed:
+  set_seed(seed)
+  
+  
+  # par: ---- 
+  
+  opar <- par(no.readonly = TRUE)
+  
+  if (is.null(col_par)){ # set default:
+    col_par <- grDevices::grey(2/3, alpha = 1)  # NA hides border/subtitle
+  }
+  
+  if ( (alpha < 1) & (is.na(col_par) == FALSE) ){
+    col_par <- usecol(col_par, alpha = alpha)  # apply alpha
+  }
+  
+  par(col = col_par)
+  
+  par(mar = c(4, 3, 3, 2) + 0.1) # default: c(5, 4, 4, 2) + 0.1
+  
+  # Create data: ---- 
+  
+  N <- n  # Note: x has N + 1 levels.
+  
+  # Initialize:
+  x <- 0:N
+  y_0 <- rep(0, length(x))
+  
+  x_plus <- c(x, rev(x))  # prepare for polygon
+  df <- data.frame(x = x, y_0 = y_0)  # prepare df
+  
+  # Main plot: ---- 
+  
+  x_min <- 0
+  x_max <- N
+  
+  y_min <- 0
+  y_max <- n_col
+  
+  col_brd <- col_par 
+  
+  # Prepare plot:
+  # plot(x = 0, type = "n", 
+  #      xlim = c(x_min, x_max), 
+  #      ylim = c(y_min, y_max), 
+  #      xlab = "X-value", ylab = "Y-value", 
+  #      main = "A polygon plot")
+  
+  plot(0, type = "n",     # (a) empty plot
+       # x = dt, col = col_pal,  # (b) generic plot
+       xlim = c(x_min, x_max), ylim = c(y_min, y_max),
+       main = NA, xlab = NA, ylab = NA
+  )
+  
+  # Create n_col - 1 non-overlapping lines in range:
+  for (i in 1:(n_col - 1)){
+    
+    # Maximum variability:
+    if (n_col < 5){
+      max_var <- 1/5  # constant
+    } else {
+      max_var <- 2/i  # mountain range (rugged front, smoother back) 
+    }
+    
+    y <- i + round(c(0, cumsum(runif(N, -max_var, +max_var))), 2)
+    
+    # Correct min and max y:
+    y[y < y_min] <- y_min  
+    y[y > y_max] <- y_max
+    
+    y[y < y_0] <- y_0[y < y_0]  # do not drop below previous/lower line y_0
+    # y[y > (i + 6)] <- (i + 6) # impose upper limit
+    
+    df[paste0("y_", i)] <- y  # record current y value
+    
+    y_plus <- c(y, rev(y_0))  # prepare for polygon
+    polygon(x = x_plus, y = y_plus, col = col_pal[i], border = col_brd) 
+    
+    y_0 <- y  # remember previous/last/lower y-values
+    
+  }
+  
+  # lines(x = x, y = y_0, lwd = 2) 
+  
+  # Final polygon:
+  y <- rep(y_max, length(x))
+  df[paste0("y_", n_col)] <- y
+  
+  y_plus <- c(y, rev(y_0))  # prepare for polygon
+  polygon(x = x_plus, y = y_plus, col = col_pal[n_col], border = col_brd)
+  
+  
+  # # Background rectangle (if axes == FALSE):
+  # bwd <- .06  # border width
+  # rect(xleft = (x_min - bwd), ybottom = (y_min - bwd), 
+  #      xright = (x_max + bwd), ytop = (y_max + bwd), 
+  #      border = col_par, xpd = TRUE)
+  
+  # Titles:
+  if (is.null(main)){ # default title:
+    plot_type <- "polygon"
+    main <- paste("A", plot_type, "plot")
+  }
+  
+  title(main = main)
+  
+  if (is.null(sub)){ # default subtitle:
+    if (is.null(pal_name)){
+      pal_name <- deparse(substitute(pal))  # get object name (of input pal)
+    }
+    sub <- paste("Illustrating color palette", pal_name)
+  }
+  mtext(sub, side = 1, line = 2, adj = 1, cex = .95)
+  
+  
+  # Output: ---- 
+  
+  on.exit(par(opar))
+  
+  return(invisible(df))
+  
+} # plot_polygon().
+
+# # Check:
+# plot_polygon(pal_unikn)
+# plot_polygon(c("steelblue", "gold", "firebrick", "black"))
+# plot_polygon(pal = Seeblau, col_par = usecol(Pinky), seed = 1)  # special case
+# x <- plot_polygon(c(rev(pal_seeblau), pal_pinky), col_par = NA, alpha = 1, n = 50)
+# head(x)
+
+
+# plot_table: An area/mosaic plot ---- 
 
 plot_table <- function(pal, col_par = NULL, alpha = 1, 
                        n = 20,  # scaling: instances per dimension is n * n_col
@@ -263,7 +420,7 @@ plot_table <- function(pal, col_par = NULL, alpha = 1,
 
 
 
-# plot_scatter: ---- 
+# plot_scatter: A plot of points ---- 
 
 #' @importFrom stats runif 
 
@@ -392,9 +549,8 @@ plot_scatter <- function(pal, col_par = NULL, alpha = 1,
 #'   \item \code{n} A scaling parameter (for random data generation);  
 #' 
 #'   \item \code{main} plot title (on top); 
-#'   \item \code{sub} subtitle (on right margin);  
+#'   \item \code{sub} plot subtitle (on right margin);  
 #'   
-#'   \item \code{pal_name} The name of input color palette \code{pal}; 
 #'   \item \code{seed} A random seed (for reproducible randomness). 
 #'   
 #' }
@@ -414,14 +570,23 @@ plot_scatter <- function(pal, col_par = NULL, alpha = 1,
 #' @param type The type of plot to be used (as character or integer index). 
 #' Permissible types are \code{"bar"}, \code{"mosaic"}, \code{"scatter"} (or 1 to 3).
 #' 
+#' @param pal_name A name for the input color palette \code{pal} (shown on bottom-right margin). 
+#' Default: \code{pal_name = NULL} (deparsing to input name).
+#' 
 #' @param ... Auxiliary arguments passed to \code{type}-specific plots (see details). 
 #' 
 #' @return The random data that was plotted (as an invisible R object). 
 #' 
 #' @examples
-#' demopal(pal_petrol, type = 1)
-#' demopal(c(rev(pal_pinky), pal_seeblau), type = 2)
-#' demopal(type = "scatter", col_par = "black", n = 100, cex = 10, seed = 101)
+#' demopal(pal = pal_petrol, type = 1)
+#' 
+#' my_pal <- c(rev(pal_pinky), pal_seeblau) 
+#' # Selecting plot type:
+#' demopal(my_pal, type = 2)          # by numeric index
+#' demopal(my_pal, type = "polygon")  # by name
+#' 
+#' # Passing type-specific arguments:
+#' demopal(type = "scatter", col_par = "black", n = 200, cex = c(2, 4, 6), seed = 101)
 #' 
 #' @family color functions
 #' 
@@ -440,10 +605,10 @@ plot_scatter <- function(pal, col_par = NULL, alpha = 1,
 #' 
 #' @export 
 
-demopal <- function(pal = pal_unikn, type = NA, ...){
+demopal <- function(pal = pal_unikn, type = NA, pal_name = NULL, ...){
   
   # Prepare: ---- 
-  plot_types <- c("bar", "mosaic", "scatter")  # as constant
+  plot_types <- c("bar", "mosaic", "polygon", "scatter")  # as constant
   
   # type: 
   if (is.character(type)){
@@ -461,7 +626,9 @@ demopal <- function(pal = pal_unikn, type = NA, ...){
     type <- sample(1:length(plot_types), size = 1)  # random type
   }
   
-  pal_name <- deparse(substitute(pal))  # get object name (of input pal)
+  if (is.null(pal_name)){
+    pal_name <- deparse(substitute(pal))  # get object name (of input pal)
+  }
   
   # Main: ---- 
   
@@ -470,10 +637,12 @@ demopal <- function(pal = pal_unikn, type = NA, ...){
          plot_bar(pal = pal, pal_name = pal_name, ...), 
          # 2: mosaic/table:
          plot_table(pal = pal, pal_name = pal_name, ...), 
-         # 3: scatter/point:
+         # 3: polygon:
+         plot_polygon(pal = pal, pal_name = pal_name, ...), 
+         # 4: scatter/point:
          plot_scatter(pal = pal, pal_name = pal_name, ...), 
          # else:
-         plot_bar(pal = pal, pal_name = pal_name, ...)
+         plot_polygon(pal = pal, pal_name = pal_name, ...)
   )
   
   # Output: None ----
