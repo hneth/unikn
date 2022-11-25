2693ebfbdc73040a719a52c1401ae0fbe333556f
hneth <hansneth@gmail.com>
2022-11-25 16:01:44 +0100

    add 3 UR palettes

--- R/add_pals.R
+++ R/add_pals.R
@@ -14,19 +14,22 @@
 
 # Institution names and URLs of add_pals:
 
-# add_pals <- c("eth_pal", "eth_pal_light", "eth_pal_grey", 
-#               "mpg_pal", 
+# add_pals <- c("eth_pal", "eth_pal_light", "eth_pal_grey",
+#               "mpg_pal",
 #               "uni_freiburg_br", "uni_freiburg_blue", "uni_freiburg_grey", "uni_freiburg_info",
-#               "uni_goettingen_1", "uni_goettingen_2", "uni_goettingen_3", 
+#               "uni_goettingen_1", "uni_goettingen_2", "uni_goettingen_3",
 #               "uni_konstanz", "uni_konstanz_pref",
-#               "uni_princeton_0", "uni_princeton_1", "uni_princeton_2")
+#               "uni_princeton_0", "uni_princeton_1", "uni_princeton_2",
+#               "uni_regensburg_1", "uni_regensburg_2", "uni_regensburg_3"
+# )
 
 inst <- c(rep("ETH Zurich", 3),
           "Max Planck Society",
           rep("University of Freiburg", 4),
           rep("University of Goettingen", 3),          
           rep("University of Konstanz", 2),
-          rep("Princeton University", 3)
+          rep("Princeton University", 3),
+          rep("University of Regensburg", 3)
 )
 
 inst_alt <- c(rep("Eidgen\u00F6ssische Technische Hochschule, Z\u00FCrich", 3),
@@ -34,7 +37,8 @@ inst_alt <- c(rep("Eidgen\u00F6ssische Technische Hochschule, Z\u00FCrich", 3),
               rep("Universit\u00E4t Freiburg", 4),
               rep("Universit\u00E4t G\u00F6ttingen", 3),
               rep("Universit\u00E4t Konstanz", 2), 
-              rep("Princeton University", 3)
+              rep("Princeton University", 3), 
+              rep("Universit\u00E4t Regensburg", 3)
 )
 
 url <- c(rep("https://ethz.ch/", 3), 
@@ -42,7 +46,8 @@ url <- c(rep("https://ethz.ch/", 3),
          rep("https://uni-freiburg.de/", 4), 
          rep("https://uni-goettingen.de/", 3), 
          rep("https://www.uni-konstanz.de/", 2), 
-         rep("https://www.princeton.edu/", 3)
+         rep("https://www.princeton.edu/", 3),
+         rep("https://www.uni-regensburg.de/", 3)
 )
 
 uni_data <- data.frame(inst = inst, inst_alt = inst_alt, url = url, pal = add_pals)
@@ -1019,7 +1024,9 @@ uni_princeton_2 <- newpal(col = c(orange_black, "black", "white"),
 # Source: <https://www.uni-regensburg.de/verwaltung/corporate-design/index.html>
 # Last update: 2022-11-25
 
-# # Service-Einrichtungen: ---- 
+# 142.142.141 : Neutralgrau : UR-Hausfarbe / Logo 
+
+# # Service-Einrichtungen: 
 # 
 # 29.63.75 : Blaugrau : Leitung, Organe und Verwaltung (LOV) [Ausnahme: Referate I/3, I/4, I/5, I/6]
 # 95.0.47 : Schwarzrot : Chancengleichheit und Familie [Familien-Service, Frauenbeauftragte, Frauenförderung, Gleichstellung]
@@ -1028,15 +1035,14 @@ uni_princeton_2 <- newpal(col = c(orange_black, "black", "white"),
 # 3.35.82 : Saphirblau : Rechenzentrum (RZ)
 # 65.98.36 : Farngrün : Zentrum für Weiterbildung
 # 
-# # Fakultätsübergreifende Einrichtungen: ---- 
-# 
+# # Fakultätsübergreifende Einrichtungen: 
 # 114.75.81 : Rotbraun : Zentrum für Sprache und Kommunikation (ZSK)
 # 86.78.111 : Perlviolett : Europaeum (Ost-West-Zentrum)
 # 59.0.65 : Violettrot : Zentrum für Hochschul- und Wissenschaftsdidaktik (ZHW)
 # 134.104.0 : Grünbraun : Regensburger Universitätszentrum für Lehrerbildung (RUL)
 # 0.129.123 : Wasserblau : Sportzentrum (SZ)
 # 
-# # Fakultäten: ---- 
+# # Fakultäten: 
 # 
 # 205.211.15 : Urangelb : Rechtswissenschaft
 # 174.167.0 : Lärchennadelgrün : Wirtschaftswissenschaften
@@ -1059,6 +1065,8 @@ uni_princeton_2 <- newpal(col = c(orange_black, "black", "white"),
 # +++ here now +++ 
 
 # Colors: ---- 
+
+UR_neutralgrau <- rgb(142, 142, 141, names = "neutralgrau",    maxColorValue = 255) # UR-Hausfarbe, Logo
 
 UR_blaugrau <-    rgb( 29,  63,  75, names = "blaugrau",    maxColorValue = 255) # Leitung, Organe und Verwaltung (LOV) 
 UR_schwarzrot <-  rgb( 95,   0,  47, names = "schwarzrot",  maxColorValue = 255) # Chancengleichheit und Familie
@@ -1121,12 +1129,116 @@ UR_glutrot_f <-            "#DA3C43" # Informatik und Data Science (TODO: Same a
 
 # Service und Fakultätsübergreifende Einrichtungen: ---- 
 
-uni_regensburg_1 <- newpal(col = c(UR_blaugrau, UR_schwarzrot, UR_schilfgruen, 
+#' General colors of the University of Regensburg.
+#'
+#' \code{uni_regensburg_1} provides the general (service-oriented) colors 
+#' of the \href{https://www.uni-regensburg.de/}{University of Regensburg}, Germany. 
+#' 
+#' Named colors of \code{uni_regensburg_1} correspond to the following services:
+#' 
+#' \enumerate{
+#' \item neutralgrau: UR-Hausfarbe, Logo 
+#' \item blaugrau: Leitung, Organe und Verwaltung (LOV, Ausnahme: Referate I/3, I/4, I/5, I/6)
+#' \item schwarzrot: Chancengleichheit und Familie (Familien-Service, Frauenbeauftragte, Frauenfoerderung, Gleichstellung)
+#' \item schilfgruen: Service-Einrichtungen der Verwaltung für Studierende (Akademisches Auslandsamt, Studentenkanzlei, Zentrales Pruefungssekretariat, Zentrale Studienberatung, Senatsbeauftragter für Behinderte und chronisch Kranke) 
+#' \item dianthusrot: Universitaetsbibliothek (UB)
+#' \item saphirblau: Rechenzentrum (RZ)
+#' \item farngruen: Zentrum fuer Weiterbildung
+#' \item rotbraun: Zentrum fuer Sprache und Kommunikation (ZSK)
+#' \item perlviolett: Europaeum (Ost-West-Zentrum)
+#' \item violettrot: Zentrum fuer Hochschul- und Wissenschaftsdidaktik (ZHW)
+#' \item gruenbraun: Regensburger Universitätszentrum fuer Lehrerbildung (RUL)
+#' \item wasserblau: Sportzentrum (SZ)
+#' }
+#' 
+#' When coloring large areas, named colors can be used in shades of 70%.
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' Hansjoerg Neth, 2022-11-25.
+#' 
+#' @source 
+#' Color definitions are based on 
+#' \url{https://www.uni-regensburg.de/verwaltung/corporate-design/index.html}. 
+#' 
+#' @examples
+#' uni_regensburg_1
+#' seecol(uni_regensburg_1)  # view color palette
+#' demopal(uni_regensburg_1, alpha = .70, type = 2, main = "General colors of the University of Regensburg")
+#'
+#' @family contributed color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_regensburg_2}} and \code{\link{uni_regensburg_3}} for the faculty colors of the University of Regensburg;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
+#' 
+#' @export
+
+uni_regensburg_1 <- newpal(col = c(UR_neutralgrau, 
+                                   UR_blaugrau, UR_schwarzrot, UR_schilfgruen, 
                                    UR_dianthusrot, UR_saphirblau, UR_farngruen,
                                    UR_rotbraun, UR_perlviolett, UR_violettrot, 
                                    UR_gruenbraun, UR_wasserblau), as_df = FALSE)
 
 # Fakultäten / web area colors: ---- 
+
+#' Faculty colors of the University of Regensburg (for areas).
+#'
+#' \code{uni_regensburg_2} provides the faculty (departmental) colors 
+#' for coloring print or web areas 
+#' at the \href{https://www.uni-regensburg.de/}{University of Regensburg}, Germany. 
+#' 
+#' Named colors of \code{uni_regensburg_2} correspond to the following faculties:
+#' 
+#' \enumerate{
+#' \item urangelb: Rechtswissenschaft
+#' \item laerchennadelgruen: Wirtschaftswissenschaften
+#' \item vatikangelb: Katholische Theologie
+#' \item orangerot: Philosophie, Kunst-, Geschichts- und Gesellschaftswissenschaften
+#' \item heucherarot: Humanwissenschaften
+#' \item heidenelkenrot: Sprach-, Literatur- und Kulturwissenschaften
+#' \item blattgruen: Biologie und Vorklinische Medizin
+#' \item tuerkisgruen: Mathematik
+#' \item eisvogelblau: Physik
+#' \item spektralblau: Chemie und Pharmazie
+#' \item capriblau: Medizin
+#' \item glutrot: Informatik und Data Science
+#' }
+#' 
+#' When coloring large areas, named colors can be used in shades of 70%.
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' Hansjoerg Neth, 2022-11-25.
+#' 
+#' @source 
+#' Color definitions are based on 
+#' \url{https://www.uni-regensburg.de/verwaltung/corporate-design/index.html}. 
+#' 
+#' @examples
+#' uni_regensburg_2
+#' seecol(uni_regensburg_2)  # view color palette
+#' demopal(uni_regensburg_2, type = 3, main = "Departmental colors of the University of Regensburg")
+#'
+#' @family contributed color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_regensburg_1}} and \code{\link{uni_regensburg_3}} for alternative colors of the University of Regensburg;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
+#' 
+#' @export
 
 uni_regensburg_2 <- newpal(col = c(UR_urangelb, UR_laerchennadelgruen, UR_vatikangelb, 
                                    UR_orangerot, UR_heucherarot, UR_heidenelkenrot, 
@@ -1143,6 +1255,56 @@ uni_regensburg_2_a <- newpal(col = c(UR_urangelb_a, UR_laerchennadelgruen_a, UR_
 all.equal(uni_regensburg_2, uni_regensburg_2_a)  # RGB values correspond to web colors for areas.
 
 # Fakultäten / web font colors: ---- 
+
+#' Faculty colors of the University of Regensburg (for fonts).
+#'
+#' \code{uni_regensburg_3} provides the faculty (departmental) colors 
+#' for web fonts 
+#' at the \href{https://www.uni-regensburg.de/}{University of Regensburg}, Germany. 
+#' 
+#' Named colors of \code{uni_regensburg_3} correspond to the following faculties:
+#' 
+#' \enumerate{
+#' \item urangelb: Rechtswissenschaft
+#' \item laerchennadelgruen: Wirtschaftswissenschaften
+#' \item vatikangelb: Katholische Theologie
+#' \item orangerot: Philosophie, Kunst-, Geschichts- und Gesellschaftswissenschaften
+#' \item heucherarot: Humanwissenschaften
+#' \item heidenelkenrot: Sprach-, Literatur- und Kulturwissenschaften
+#' \item blattgruen: Biologie und Vorklinische Medizin
+#' \item tuerkisgruen: Mathematik
+#' \item eisvogelblau: Physik
+#' \item spektralblau: Chemie und Pharmazie
+#' \item capriblau: Medizin
+#' \item glutrot: Informatik und Data Science
+#' }
+#' 
+#' Note that the font color glutrot is currently identical to the area color of \code{\link{uni_regensburg_2}}.
+#' 
+#' @return 
+#' A named vector of colors (HEX/HTML codes of type character).
+#' 
+#' @author 
+#' Hansjoerg Neth, 2022-11-25.
+#' 
+#' @source 
+#' Color definitions are based on 
+#' \url{https://www.uni-regensburg.de/verwaltung/corporate-design/index.html}. 
+#' 
+#' @examples
+#' uni_regensburg_3
+#' seecol(uni_regensburg_3)  # view color palette
+#' demopal(uni_regensburg_3, type = 4, seed = 13, main = "Departmental colors of the University of Regensburg")
+#'
+#' @family contributed color palettes
+#'
+#' @seealso 
+#' \code{\link{uni_regensburg_1}} and \code{\link{uni_regensburg_2}} for alternative colors of the University of Regensburg;  
+#' \code{\link{seecol}} for viewing and comparing color palettes; 
+#' \code{\link{usecol}} for using color palettes; 
+#' \code{\link{simcol}} for finding similar colors; 
+#' \code{\link{newpal}} for defining new color palettes; 
+#' \code{\link{grepal}} for finding named colors. 
 
 uni_regensburg_3 <- newpal(col = c(UR_urangelb_f, UR_laerchennadelgruen_f, UR_vatikangelb_f,
                                    UR_orangerot_f, UR_heucherarot_f, UR_heidenelkenrot_f,
