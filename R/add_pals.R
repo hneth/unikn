d545c175d7c23f997eb80f44dd0c5f217928bc3a
Hansjörg Neth <hansneth@gmail.com>
2022-12-06 09:16:21 +0100

    add Willamette colors

--- R/add_pals.R
+++ R/add_pals.R
@@ -2737,6 +2737,58 @@ rpi_pal_3 <- newpal(col = c("#ab2328", "#c35442", "#d58570", "#eabcad",
 # seecol(rpi_pal_3, main = "Tint Colors of RPI")
 
 
+# Willamette University: -----
+
+# Source: <https://willamette.edu/offices/comm/brand-guidelines/brand-colors/index.html>
+# Last check: 2022-12-05
+
+# Primary Colors:
+#
+# Cardinal
+# PANTONE 200 C
+# PROCESS: 0/100/76/13
+# RGB: 186/12/47
+# HEX: BA0C2F
+# 
+# Gold
+# PANTONE 466 C
+# PROCESS: 13/25/52/6
+# RGB: 198/170/118
+# HEX: C6AA76
+
+# Secondary Colors:
+#
+# Midnight Breakfast
+# PANTONE 302 C
+# PROCESS: 100/32/0/68
+# RGB: 0/59/92
+# HEX: 003B5C
+# 
+# Waller Brick
+# PANTONE 202 C
+# PROCESS: 1/98/58/44
+# RGB: 156/2/33
+# HEX: 9C0221
+# 
+# Zena Field
+# PANTONE 4009 C
+# PROCESS: 0/42/76/0
+# RGB: 241/156/73
+# HEX: F19C49
+# 
+# Salem Sky
+# PANTONE 3105
+# PROCESS: 49/0/7/0
+# RGB: 103/210/223
+# HEX: 67D2DF
+
+willamette_pal <- newpal(col = c("#BA0C2F", "#C6AA76",  "#003B5C", "#9C0221", "#F19C49", "#67D2DF"),
+                         names = c("Cardinal", "Gold",  "Midnight breakfast", "Waller brick", "Zena field", "Salem sky"),
+                         as_df = FALSE)
+
+# seecol(willamette_pal, main = "Primary and secondary colors of Willamette University")
+
+
 ## ToDo: -------- 
 
 # - Uni Heidelberg: <https://www.uni-heidelberg.de/de/universitaet/das-profil-der-universitaet-heidelberg/corporate-design>
