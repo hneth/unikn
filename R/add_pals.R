dd514241f3e361d105a101438ceffebf9205a888
Hansjörg Neth <hansneth@gmail.com>
2023-01-02 12:01:37 +0100

    bug fix: avoid using Umlauts in documentation

--- R/add_pals.R
+++ R/add_pals.R
@@ -2588,7 +2588,7 @@ kiel_main <- "#9b0a7d"  # = rgb(155, 10, 125, maxColorValue = 255)
 #' for creating color gradients. 
 #' 
 #' The full name of \href{https://www.uni-kiel.de/en/}{Kiel University} 
-#' is Christian-Albrechts-Universität (CAU) zu Kiel.
+#' is Christian-Albrechts-Universit\u00E4t (CAU) zu Kiel. 
 #' 
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
@@ -2695,7 +2695,7 @@ kiel_faculty_names <- c("Theologie", "Rechtswissenschaft", "Medizin", "Philosoph
 #' for creating color gradients. 
 #' 
 #' The full name of \href{https://www.uni-kiel.de/en/}{Kiel University} 
-#' is Christian-Albrechts-Universität (CAU) zu Kiel.  
+#' is Christian-Albrechts-Universit\u00E4t (CAU) zu Kiel.  
 #' 
 #' @return 
 #' A named vector of colors (HEX/HTML codes of type character).
