3fb085bc40ff24c869f432344bae8d9448609d71
hneth <hansneth@gmail.com>
2023-09-19 16:27:55 +0200

    explore base::substitute() and rlang::enexpr()

--- R/util.R
+++ R/util.R
@@ -1,5 +1,5 @@
 ## util.R | unikn
-## spds | uni.kn |  2023 08 15
+## spds | uni.kn |  2023 09 19
 ## ---------------------------
 
 # General utility functions (mostly for internal use and not exported).
@@ -20,7 +20,7 @@ add_quotes <- function(x) {
 
 # - monotonic: Check if a vector is monotonically increasing ------ 
 
-monotonic <-function(v) {
+monotonic <- function(v) {
   
   mono <- NA
   
@@ -117,6 +117,22 @@ url_unikn <- function(url = "https://www.uni-konstanz.de/"){
 # url_unikn()
 # url_unikn("https://www.spds.uni-konstanz.de/")
 # url_unikn("https://www.uni-konstanz.de/www/https/http/_test_//")
+
+
+
+
+# - get_args: Get arguments of a function ------
+
+# get_list_args <- function(x){
+# 
+#   base::substitute(x)
+#   
+#   # rlang::enexpr(x)
+# 
+# } # get_list_args().
+
+# # Check:
+# as.character(get_list_args(x = list(a, b, c)))[-1]
 
 
 
